60ac9299414e3f80d9e957c4b802e4b0cdf56f8b
yusekim <youwin0802@naver.com>
2025-01-02 19:56:49 +0900

    done but need some testing

--- modules.py
+++ modules.py
@@ -9,7 +9,6 @@ class Polynomial:
 	def __init__(self, terms):
 		self.terms = terms # Term 배열
 		self.degree = terms[-1].exponent # 가장 큰 차수
-		self.is_unexpected_exponent = False # 음수 또는 소수 차수가 포함시 True
 		self.is_solution_is_real = False # 해가 모든 실수일 경우 True
 
 	def __str__(self):
@@ -36,6 +35,46 @@ class Polynomial:
 		expo_str = str(int(self.degree) if self.degree == int(self.degree) else self.term[-1].exponent)
 		print(f"Polynomial degree: {expo_str}")
 
+	def calculate(self):
+		if self.is_solution_is_real:
+			print("All real number is a solution...")
+			exit()
+		self.is_calculatable()
+		if self.degree == 1:
+			self.calculate_oneD()
+		elif self.degree == 2:
+			self.calculate_twoD()
+
+	def is_calculatable(self):
+		for item in self.terms:
+			if item.exponent > 2:
+				print("The polynomial degree is strictly greater than 2, I can't solve.")
+				exit()
+			elif item.exponent != int(item.exponent) or item.exponent < 0:
+				print("Unexpected polynomial degree detected, I can't solve.")
+				exit()
+
+	def calculate_twoD(self):
+		a = self.terms[2].coefficient
+		b = self.terms[1].coefficient
+		c = self.terms[0].coefficient
+		D = b * b - 4 * a * c
+		if D > 0:
+			print("Discriminant is strictly positive, the two solutions are:")
+			ans1 = (-b - ft_math.sqrt(D)) / (2 * a)
+			ans2 = (-b + ft_math.sqrt(D)) / (2 * a)
+			print(round(ans1, 6), round(ans2, 6), sep="\n")
+		elif D < 0:
+			print("Discriminant is strictly negative, I can't solve.")
+			exit()
+		else:
+			print("Discriminant is strictly zero, the solution is:")
+			print(-b / 2 *a)
+
+
+	def calculate_oneD(self):
+		solution = self.terms[0].coefficient / self.terms[-1].coefficient * -1
+		print("The solution is:", solution, sep='\n')
 
 	@staticmethod
 	def format_term(coefficient, exponent):
